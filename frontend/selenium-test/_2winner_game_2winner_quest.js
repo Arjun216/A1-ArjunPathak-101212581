f5fc9083bf36f0785b7c0af449db4d2af6d4d891
Arjun216 <78912850+Arjun216@users.noreply.github.com>
2024-12-06 17:22:58 -0500

    Update _2winner_game_2winner_quest.js

--- frontend/selenium-test/_2winner_game_2winner_quest.js
+++ frontend/selenium-test/_2winner_game_2winner_quest.js
@@ -141,9 +141,9 @@ async function simulateUserInputs(driver) {
 
         await inputField.clear();
         await inputField.sendKeys(input);
-        await new Promise(resolve => setTimeout(resolve, 50));
+        await new Promise(resolve => setTimeout(resolve, 700));
         await sendButton.click();
-        //await new Promise(resolve => setTimeout(resolve, 300));
+        await new Promise(resolve => setTimeout(resolve, 300));
 
         if (index === 0) await assertSponsor('P1');
         if (index === 12) {
